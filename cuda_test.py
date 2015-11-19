8f8a10784f9b336f4c4005ecb31627dd06dcec38
brett <brett_graham@hms.harvard.edu>
2015-11-19 15:29:17 -0500

    adding average time stats for cuda_test

--- cuda_test.py
+++ cuda_test.py
@@ -2,14 +2,15 @@
 
 import time
 
+import numpy
 import pycuda.driver
 import pylab
 
 import springmesh
 
-#size = (500, 500)
+size = (1000, 1000)
 #size = (200, 200)
-size = (100, 100)
+#size = (100, 100)
 n = 100
 bs = (1024, 1, 1)
 show = False
@@ -35,12 +36,14 @@ print("mem(free): %s" % (free * 100 / float(total)))
 print("springs[n]: %s" % (len(gm.springs), ))
 print("points[n]: %s" % (len(gm.points), ))
 i = 0
+ts = []
 while n_iters > 0:
     n_iters -= 1
     t0 = time.time()
     springmesh.relax.cuda.step_n(gm, n=n, s=s)
     t1 = time.time()
     i += n
+    ts.append(t1 - t0)
 
     print(
         "i: %s, size: %s, n: %s, gpu[%s]: %s" % (
@@ -57,3 +60,8 @@ while n_iters > 0:
     pylab.title("gpu: %s" % i)
     pylab.show()
 gm = springmesh.relax.cuda.finalize(gm)
+
+avg_t = numpy.mean(ts)
+print("Size: %s" % (size, ))
+print("Average time per run[%s]: %s" % (n, avg_t))
+print("Average time per step: %s" % (avg_t / float(n), ))
